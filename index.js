b91a51bdf7eacc7b8a2eb5735c978dd32f088a4e
Nur Shalahuddin Fajri <shotozuro@gmail.com>
2020-07-09 05:36:51 +0700

    add npm script

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ app.get('/home', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`listening at http://${BASE_URL}:${port}`);
+  console.log(`listening at http://localhost:${port}`);
 });
